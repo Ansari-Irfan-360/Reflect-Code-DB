85d342d81ddd6f7ca30350f47ee340c1879b4acd
Ansari Irfan <115148633+Ansari-Irfan-360@users.noreply.github.com>
2024-03-13 18:36:11 +0530

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -45,10 +45,10 @@ function Home() {
           <div className="card shadow-sm p-2 mb-5 bg-secondary rounded">
             <div className="card-body text-center bg-dark">
               <img
-                src="/images/codecast.png"
+                src="/reflective-code.png"
                 alt="Logo"
                 className="img-fluid mx-auto d-block"
-                style={{ maxWidth: "150px" }}
+                style={{ maxWidth: "550px" }}
               />
               <h4 className="card-title text-light mb-4">Enter the ROOM ID</h4>
 
