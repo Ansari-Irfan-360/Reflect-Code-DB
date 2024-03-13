44dd3b7c7412b0bca6be537cf148e27390e9a14c
Ansari Irfan <115148633+Ansari-Irfan-360@users.noreply.github.com>
2024-03-13 18:21:19 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import EditorPage from './components/EditorPage';
+import { Toaster } from 'react-hot-toast';
+
+function App() {
+  return (
+    <>
+    <div>
+      <Toaster  position='top-center'></Toaster>
+    </div>
+    <Routes>
+     <Route path='/' element={ <Home /> } />
+     <Route path='/editor/:roomId' element={ <EditorPage /> } />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/EditorPage.js
@@ -0,0 +1,147 @@
+import React, { useEffect, useRef, useState } from "react";
+import Client from "./Client";
+import Editor from "./Editor";
+import { initSocket } from "../Socket";
+import { ACTIONS } from "../Actions";
+import {
+  useNavigate,
+  useLocation,
+  Navigate,
+  useParams,
+} from "react-router-dom";
+import { toast } from "react-hot-toast";
+
+function EditorPage() {
+  const [clients, setClients] = useState([]);
+  const codeRef = useRef(null);
+
+  const Location = useLocation();
+  const navigate = useNavigate();
+  const { roomId } = useParams();
+
+  const socketRef = useRef(null);
+  useEffect(() => {
+    const init = async () => {
+      socketRef.current = await initSocket();
+      socketRef.current.on("connect_error", (err) => handleErrors(err));
+      socketRef.current.on("connect_failed", (err) => handleErrors(err));
+
+      const handleErrors = (err) => {
+        console.log("Error", err);
+        toast.error("Socket connection failed, Try again later");
+        navigate("/");
+      };
+
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        username: Location.state?.username,
+      });
+
+      // Listen for new clients joining the chatroom
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          // this insure that new user connected message do not display to that user itself
+          if (username !== Location.state?.username) {
+            toast.success(`${username} joined the room.`);
+          }
+          setClients(clients);
+          // also send the code to sync
+          socketRef.current.emit(ACTIONS.SYNC_CODE, {
+            code: codeRef.current,
+            socketId,
+          });
+        }
+      );
+
+      // listening for disconnected
+      socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, username }) => {
+        toast.success(`${username} left the room`);
+        setClients((prev) => {
+          return prev.filter((client) => client.socketId !== socketId);
+        });
+      });
+    };
+    init();
+
+    // cleanup
+    return () => {
+      socketRef.current && socketRef.current.disconnect();
+      socketRef.current.off(ACTIONS.JOINED);
+      socketRef.current.off(ACTIONS.DISCONNECTED);
+    };
+  }, []);
+
+  if (!Location.state) {
+    return <Navigate to="/" />;
+  }
+
+  const copyRoomId = async () => {
+    try {
+      await navigator.clipboard.writeText(roomId);
+      toast.success(`roomIs is copied`);
+    } catch (error) {
+      console.log(error);
+      toast.error("unable to copy the room Id");
+    }
+  };
+
+  const leaveRoom = async () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="container-fluid vh-100">
+      <div className="row h-100">
+        {/* client panel */}
+        <div
+          className="col-md-2 bg-dark text-light d-flex flex-column h-100"
+          style={{ boxShadow: "2px 0px 4px rgba(0, 0, 0, 0.1)" }}
+        >
+          <img
+            src="/images/codecast.png"
+            alt="Logo"
+            className="img-fluid mx-auto"
+            style={{ maxWidth: "150px", marginTop: "-43px" }}
+          />
+          <hr style={{ marginTop: "-3rem" }} />
+
+          {/* Client list container */}
+          <div className="d-flex flex-column flex-grow-1 overflow-auto">
+            <span className="mb-2">Members</span>
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
+
+          <hr />
+          {/* Buttons */}
+          <div className="mt-auto ">
+            <button className="btn btn-success" onClick={copyRoomId}>
+              Copy Room ID
+            </button>
+            <button
+              className="btn btn-danger mt-2 mb-2 px-3 btn-block"
+              onClick={leaveRoom}
+            >
+              Leave Room
+            </button>
+          </div>
+        </div>
+
+        {/* Editor panel */}
+        <div className="col-md-10 text-light d-flex flex-column h-100 ">
+          <Editor
+            socketRef={socketRef}
+            roomId={roomId}
+            onCodeChange={(code) => {
+              codeRef.current = code;
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EditorPage;
