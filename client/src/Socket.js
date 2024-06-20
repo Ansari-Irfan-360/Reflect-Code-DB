ab115e95a48763b17919994d6a4b05db98fb9fc1
Ansari Irfan <115148633+Ansari-Irfan-360@users.noreply.github.com>
2024-06-20 22:53:37 +0530

    Update Socket.js

--- client/src/Socket.js
+++ client/src/Socket.js
@@ -7,5 +7,5 @@ export const initSocket = async () =>{
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io("http://localhost:8000/", options);
+    return io("https://reflect-code.vercel.app", options);
 }
